4cab7c04ede80df042c19b602ca5601ee286ebbd
Ilya Kurbatov <xboxtracer@yandex.ru>
2020-11-30 22:19:08 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,6 @@
-import goblin from '../img/goblin.png'
-import './game_field.js';
+import gameField from './game_field';
+import goblinGame from './game_mind';
 
-const image = `<img src="${goblin}">`;
-
-
-const divInnerFlex = document.querySelectorAll('.flex-inner');
-let imgPlace = 0;
-
-function rndNumber() {
-    return Math.floor(Math.random() * fieldsCount);
-};
-
-setInterval(()=> {
-    divInnerFlex[imgPlace].innerHTML = '';
-    let someNumber = rndNumber();
-    while(imgPlace === someNumber) {
-        someNumber = rndNumber();
-    };
-    imgPlace = someNumber;
-    divInnerFlex[imgPlace].innerHTML = image;
-}, 1000);
+let fieldsCount = 16;
+new gameField(fieldsCount);
+new goblinGame;
