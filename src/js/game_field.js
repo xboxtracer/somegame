d8d873b4e1e46686a02e100e97b6aebfe23fc7fe
Ilya Kurbatov <xboxtracer@yandex.ru>
2020-11-30 22:19:02 +0300

    Make gamefield in OOP

--- src/js/game_field.js
+++ src/js/game_field.js
@@ -1,10 +1,16 @@
-
-const fieldsCount = 16
-const divContainer = document.querySelector('.flex-container');
-let i = 0;
-while (i < fieldsCount) {
-    i++;
-    const field = document.createElement('div');
-    field.className = 'flex-inner';
-    divContainer.appendChild(field);
-};
+export default class gameFieldRendering {
+    constructor(size) {
+        this.divContainer = document.querySelector('.flex-container'),
+        this.size = size,
+        this.render()
+    }
+    render() {
+        let i = 0;
+        while (i < this.size) {
+            i++;
+            const field = document.createElement('div');
+            field.className = 'flex-inner';
+            this.divContainer.appendChild(field);
+        };
+    }
+}
